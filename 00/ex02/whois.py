34d5928b9803ff8172d6be18ef2ba74071aeba17
Youssef Lagtab <yousseflagtab@gmail.com>
2022-02-05 02:48:36 +0100

    add day00 ex02

---
+++ 00/ex02/whois.py
@@ -0,0 +1,12 @@
+import sys
+
+args = sys.argv
+if len(args) > 2 or not args[1].isnumeric() :
+    print('ERROR')
+    sys.exit(1)
+
+number = int(args[1])
+if number % 2:
+    print("I'm Odd.")
+else:
+    print("I'm Even.")
