9c7d22744f44082c14be9cf277f9cad5e37983c9
Youssef Lagtab <yousseflagtab@gmail.com>
2022-02-14 02:18:42 +0100

    add day02 ex01

---
+++ 02/ex01/main.py
@@ -0,0 +1,45 @@
+def what_are_the_vars(*args, **kwargs):
+    o = ObjectC()
+
+    for i, val in enumerate(args):
+        setattr(o, f"var_{i}", val)
+    for key, val in kwargs.items():
+        if hasattr(o, key):
+            return None
+        setattr(o, key, val)
+
+    return o
+
+
+class ObjectC(object):
+    def __init__(self):
+        pass
+
+
+def doom_printer(obj):
+    if obj is None:
+        print("ERROR")
+        print("end")
+        return
+    for attr in dir(obj):
+        if attr[0] != '_':
+            value = getattr(obj, attr)
+            print("{}: {}".format(attr, value))
+    print("end")
+
+
+if __name__ == "__main__":
+    obj = what_are_the_vars(7)
+    doom_printer(obj)
+    obj = what_are_the_vars(None, [])
+    doom_printer(obj)
+    obj = what_are_the_vars("ft_lol", "Hi")
+    doom_printer(obj)
+    obj = what_are_the_vars()
+    doom_printer(obj)
+    obj = what_are_the_vars(12, "Yes", [0, 0, 0], a=10, hello="world")
+    doom_printer(obj)
+    obj = what_are_the_vars(42, a=10, var_0="world")
+    doom_printer(obj)
+    obj = what_are_the_vars(42, "Yes", a=10, var_2="world")
+    doom_printer(obj)
